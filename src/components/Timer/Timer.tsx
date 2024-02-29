88882c8215e04b38a57259efd9114e61490cb1fe
huanc0de <arturfurtak5@gmail.com>
2024-02-29 22:24:31 +0100

    added timer

--- src/components/Timer/Timer.tsx
+++ src/components/Timer/Timer.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import styles from './Timer.module.scss';
 
 interface IProps {
@@ -6,6 +7,27 @@ interface IProps {
 }
 
 const Timer = ({ color, setColor }: IProps) => {
+  const [seconds, setSeconds] = useState(10);
+  const [minutes, setMinutes] = useState(0);
+  const [isTicking, setIsTicking] = useState(false);
+
+  const startTimer = () => {
+    setIsTicking(true);
+  };
+  const stopTimer = () => {
+    setIsTicking(false);
+  };
+
+  const resetTimer = () => {
+    setSeconds(60);
+    setMinutes(20);
+  };
+
+  useEffect(() => {
+    const timer = setTimeout(() => isTicking && setSeconds(seconds - 1), 1e3);
+    return () => clearTimeout(timer);
+  });
+
   return (
     <div className={styles.container}>
       <button
@@ -16,9 +38,22 @@ const Timer = ({ color, setColor }: IProps) => {
             setColor('red');
           }
         }}
+        className={styles.colorBtn}
       >
-        POMODORO
+        COLOR
       </button>
+      <button onClick={() => resetTimer()} className={styles.resetBtn}>
+        RESET
+      </button>
+      <button onClick={() => startTimer()} className={styles.startBtn}>
+        START
+      </button>
+      <button onClick={() => stopTimer()} className={styles.startBtn}>
+        STOP
+      </button>
+      <p className={styles.time}>
+        {String('0' + minutes).slice(-2)}:{String('0' + seconds).slice(-2)}
+      </p>
     </div>
   );
 };
