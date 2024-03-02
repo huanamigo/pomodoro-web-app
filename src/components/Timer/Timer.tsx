f6835624bfcaf48d314e4769d21d276efede312e
huanc0de <arturfurtak5@gmail.com>
2024-03-02 13:48:16 +0100

    made timer do timer stuff

--- src/components/Timer/Timer.tsx
+++ src/components/Timer/Timer.tsx
@@ -9,27 +9,33 @@ interface IProps {
 const Timer = ({ color, setColor }: IProps) => {
   const [miliseconds, setMiliseconds] = useState(0);
   const [seconds, setSeconds] = useState(0);
-  const [minutes, setMinutes] = useState(20);
+  const [minutes, setMinutes] = useState(1);
   const [isTicking, setIsTicking] = useState(false);
   const [startDate, setStartDate] = useState(new Date().getTime());
+  const [stoppedMiliseconds, setStoppedMiliseconds] = useState(0);
 
   const startTimer = () => {
     setStartDate(new Date().getTime());
     setIsTicking(true);
+    setMiliseconds(miliseconds);
   };
   const stopTimer = () => {
     setIsTicking(false);
+    setStoppedMiliseconds(miliseconds);
   };
 
   const resetTimer = () => {
+    stopTimer();
     setMiliseconds(0);
-    setSeconds(60);
-    setMinutes(20);
+    setSeconds(0);
+    setMinutes(1);
   };
 
   useEffect(() => {
     const timer = setTimeout(
-      () => isTicking && setMiliseconds(new Date().getTime() - startDate),
+      () =>
+        isTicking &&
+        setMiliseconds(new Date().getTime() - startDate + stoppedMiliseconds),
       1
     );
     return () => clearTimeout(timer);
@@ -37,12 +43,26 @@ const Timer = ({ color, setColor }: IProps) => {
 
   useEffect(() => {
     if (miliseconds >= 1000) {
-      console.log(miliseconds);
-      console.log('reset');
       setStartDate(new Date().getTime());
       setMiliseconds(0);
+      setStoppedMiliseconds(0);
+      console.log(miliseconds);
+
+      if (seconds === 0) {
+        if (minutes === 0) {
+          stopTimer();
+          console.log('koniec');
+          setMinutes(0);
+          setSeconds(0);
+        } else {
+          setMinutes(minutes - 1);
+          setSeconds(59);
+        }
+      } else {
+        setSeconds(seconds - 1);
+      }
     }
-  }, [miliseconds]);
+  }, [miliseconds, seconds, minutes]);
 
   return (
     <div className={styles.container}>
@@ -61,16 +81,25 @@ const Timer = ({ color, setColor }: IProps) => {
       <button onClick={() => resetTimer()} className={styles.resetBtn}>
         RESET
       </button>
-      <button onClick={() => startTimer()} className={styles.startBtn}>
+      <button
+        onClick={() => {
+          if (isTicking) {
+            stopTimer();
+          } else {
+            startTimer();
+          }
+        }}
+        className={styles.startBtn}
+      >
         START
       </button>
       <button onClick={() => stopTimer()} className={styles.startBtn}>
         STOP
       </button>
       <p className={styles.time}>
-        {String('0' + minutes).slice(-2)}:{String('0' + seconds).slice(-2)}:
-        {miliseconds}
+        {String('0' + minutes).slice(-2)}:{String('0' + seconds).slice(-2)}
       </p>
+      <p className={styles.time}>{String('00' + miliseconds).slice(-3)}</p>
     </div>
   );
 };
